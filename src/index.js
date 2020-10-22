69c597b856bb0cb4c8279ff45c5dceb7bcfce4da
unknown <igor.yakubovsky@gmail.com>
2020-10-22 21:49:07 +0300

    feat:add morse-decoder

--- src/index.js
+++ src/index.js
@@ -1,44 +1,72 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+    '.-': 'a',
+    '-...': 'b',
+    '-.-.': 'c',
+    '-..': 'd',
+    '.': 'e',
+    '..-.': 'f',
+    '--.': 'g',
+    '....': 'h',
+    '..': 'i',
+    '.---': 'j',
+    '-.-': 'k',
+    '.-..': 'l',
+    '--': 'm',
+    '-.': 'n',
+    '---': 'o',
+    '.--.': 'p',
+    '--.-': 'q',
+    '.-.': 'r',
+    '...': 's',
+    '-': 't',
+    '..-': 'u',
+    '...-': 'v',
+    '.--': 'w',
+    '-..-': 'x',
+    '-.--': 'y',
+    '--..': 'z',
+    '.----': '1',
+    '..---': '2',
+    '...--': '3',
+    '....-': '4',
+    '.....': '5',
+    '-....': '6',
+    '--...': '7',
+    '---..': '8',
+    '----.': '9',
+    '-----': '0',
 };
 
 function decode(expr) {
-    // write your solution here
+    let arLength = expr.match(/.{1,10}/g);
+    let newArr = arLength.map((element) => {
+        let firstIndex = element.indexOf('1');
+        return element.substring(firstIndex, element.length)
+    });
+    let array = newArr.map((element) => {
+        if (element === "**********") {
+            return " ";
+        } else {
+            let newPr = element.match(/.{1,2}/g);
+            let morze = newPr.map((newElement) => {
+                if (newElement === '10') {
+                    return '.'
+
+                } else {
+                    return '-';
+                }
+            });
+            return morze.join('');
+        }
+    });
+    let rezMoez = array.map((newElement) => {
+        if (newElement === " ") {
+            return " "
+        } else {
+            return MORSE_TABLE[newElement]
+        }
+    });
+    return rezMoez.join("");
 }
 
 module.exports = {
